7f65b3c8d2dbb7c74ebe38dc56814b7616150e9c
Gregory <gregory.ho13@gmail.com>
2022-05-16 15:57:19 -0400

    website-v1

--- coc_utils.py
+++ coc_utils.py
@@ -2,6 +2,7 @@ import re
 from bs4 import BeautifulSoup as bs4
 import asyncio
 import nest_asyncio
+from IPython.display import clear_output
 
 async def get_player_info(client, player_tag, limit=['achievements','load_game_data','builder_hall','builder_troops']):
     player = await client.get_player(player_tag)
@@ -43,9 +44,10 @@ async def get_player_info(client, player_tag, limit=['achievements','load_game_d
             player_info[attr] = data if type(data) is int or type(data) is str or type(data) is list else str(data)
     return player_info
 
-async def get_clan_info(client, clan_tag, limit=['badge','members'], detailed=False):
+async def get_clan_info(client, clan_tag, limit=None, detailed=False, omit_members=False):
     clan = await client.get_clan(clan_tag)
     
+    # Exclude badge information and members (member data is implemented elsewhere)
     if limit is None:
         limit = ['badge','members']
     else:
@@ -62,40 +64,108 @@ async def get_clan_info(client, clan_tag, limit=['badge','members'], detailed=Fa
                 data[idx] = str(elm)
         clan_info[attr] = data if type(data) is int or type(data) is str or type(data) is list else str(data)
     
+    # If detailed, collect all clan-war-useful data from each member
     if detailed:
         members = []
+        member_attack_wins = []
+        best_trophies = []
+        donations = []
+        
+        pets = ['lassi','electro_owl','mighty_yak','unicorn']
+        pet_lvls = {'lassi': [], 'electro_owl': [], 'mighty_yak': [], 'unicorn': []}
+        
+        heroes = ['barbarian_king','archer_queen','grand_warden','royal_champion']
+        hero_lvls = {'barbarian_king': [], 'archer_queen': [], 'grand_warden': [], 'royal_champion': []}
+        
+        legend_trophies = []
+        town_hall = []
+        trophies = []
+        war_stars = []
+        total_war_opted_in = 0
         async for player in clan.get_detailed_members():
-            player_info = get_player_info(client, player.tag)
-            members.append(player_info)
-        clan_info['members'] = members
+            player_info = await get_player_info(client, player.tag)
+            members.append(player.name+player.tag)
+            
+            member_attack_wins.append(player_info['attack_wins']) if player_info['attack_wins'] is not str else member_attack_wins.append(0)
+            best_trophies.append(player_info['best_trophies']) if player_info['best_trophies'] is not str else best_trophies.append(0)
+            donations.append(player_info['donations']) if player_info['donations'] is not str else donations.append(0)
+            
+            for pet in pets:
+                pet_lvls[pet].append(player_info[pet]) if pet in player_info else pet_lvls[pet].append(0)
+            for hero in heroes:
+                hero_lvls[hero].append(player_info[hero]) if hero in player_info else hero_lvls[hero].append(0)
+            
+            
+            legend_trophies.append(player_info['legend_trophies']) if 'legend_trophies' in player_info else legend_trophies.append(0)
+            
+            town_hall.append(player_info['town_hall'])
+            trophies.append(player_info['trophies'])
+            war_stars.append(player_info['war_stars'])
+            
+            total_war_opted_in += 1 if player_info['war_opted_in']=='True' else 0
+        
+        # Add sorted values list to clan_info dictionary
+        clan_info['member_attack_wins'] = sorted(member_attack_wins, reverse=True)
+        clan_info['best_trophies'] = sorted(best_trophies, reverse=True)
+        clan_info['donations'] = sorted(donations, reverse=True)
+        
+        for pet in pets:
+            clan_info[pet] = sorted(pet_lvls[pet], reverse=True)
+        for hero in heroes:
+            clan_info[hero] = sorted(hero_lvls[hero], reverse=True)
+        
+        clan_info['legend_trophies'] = sorted(legend_trophies, reverse=True)
+        clan_info['town_hall'] = sorted(town_hall, reverse=True)
+        clan_info['trophies'] = sorted(trophies, reverse=True)
+        clan_info['war_stars'] = sorted(war_stars, reverse=True)
+        clan_info['total_war_opted_in'] = total_war_opted_in
+        if not omit_members:
+            clan_info['members'] = members
         
     else:
-        clan_info['members'] = [player.name+player.tag async for player in clan.get_detailed_members()]
+        if not omit_members:
+            members = []
+            async for member in clan.get_detailed_members():
+                members.append(member.name+member.tag)
+            clan_info['members'] = members
     
     return clan_info
 
-async def get_warlog_info(client, clan_tag):
+async def get_warlog_info(client, clan_tag, clan_details=False):
     war_log = await client.get_warlog(clan_tag)
     
     war_log_info = []
     for i, war in enumerate(war_log):
-        warlog_attributes = [attr for attr in dir(war) if re.search(r'^[^_]+',attr) and not re.search(r'(_cls)$|^(get_)',attr)]
-        warlog_info = [getattr(war,attr) for attr in warlog_attributes]
-        
-        war_info = {}
-        for attr, info in zip(warlog_attributes, warlog_info):
-            if attr == 'clan' or attr == 'opponent':
-                limit = ['badge','members','attacks','defenses','is_opponent']
-                if attr == 'opponent':
-                    limit += ['exp_earned','attacks_used']
+        if not war_log[i].is_league_entry:
+            clear_output(wait=True)
+            print(f'Logging information on war against {war_log[i].opponent.name}{war_log[i].opponent.tag}...')
+            print(f'War {i+1} of {len(war_log)}')
+            warlog_attributes = [attr for attr in dir(war) if re.search(r'^[^_]+',attr) and not re.search(r'(_cls)$|^(get_)',attr) and not attr in ['attacks_per_member','is_league_entry']]
+            warlog_info = [getattr(war,attr) for attr in warlog_attributes]
 
-                clan_war_attributes = [attr for attr in dir(info) if re.search(r'^[^_]+',attr) and not re.search(r'(_cls)$|^(get_)',attr) and not attr in limit]
-                for war_attr in clan_war_attributes:
-                    war_info[attr +'_'+war_attr] = getattr(info, war_attr)
-            elif attr == 'end_time':
-                war_info[attr] = format(info.time)
-            else:
-                war_info[attr] = info
-        war_log_info.append(war_info)
-    
+            war_info = {}
+            for attr, info in zip(warlog_attributes, warlog_info):
+                if attr == 'clan' or attr == 'opponent':
+                    limit = ['badge','members','attacks','defenses','is_opponent']
+                    if attr == 'opponent':
+                        limit += ['exp_earned','attacks_used','average_attack_duration']
+
+                    clan_war_attributes = [attr for attr in dir(info) if re.search(r'^[^_]+',attr) and not re.search(r'(_cls)$|^(get_)',attr) and not attr in limit]
+                    for war_attr in clan_war_attributes:
+                        war_info[attr +'_'+war_attr] = getattr(info, war_attr)
+                elif attr == 'end_time':
+                    war_info[attr] = format(info.time)
+                else:
+                    war_info[attr] = info
+
+            if clan_details:
+                details = await get_clan_info(client, war_info['opponent_tag'], limit=['tag','name','share_link'],detailed=True, omit_members=True)
+                war_info = war_info | details
+                war_info_del = ['clan_name','clan_share_link','clan_tag','clan_average_attack_duration','labels']
+                for tag in war_info_del:
+                    del war_info[tag]
+
+            war_log_info.append(war_info)
+    clear_output()
+    print(f'Wars {i+1} of {len(war_log)} finished logging')
     return war_log_info
